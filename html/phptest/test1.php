e84b3bd90c6e1b83067a0f5e9a562ee64497f6b5
jxke05050505 <jakehancox12@gmail.com>
2024-06-17 15:57:58 +0100

    use variables with echo

--- html/phptest/test1.php
+++ html/phptest/test1.php
@@ -1,11 +1,17 @@
 <html>
     <head>
         <title>PHP Test 1</title>
+        <style>
+            body{
+                font-family: "segoe ui", 'inconsolata', consolas, monospace;
+            }
+        </style>
     </head>
     <body>
         <h1>PHP Test 1</h1>
         <?php
-            echo(1+1);
+            $test = "this is a string variable";
+            echo($test);
         ?>
     </body>
 </html>
