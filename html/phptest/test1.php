b4439b423e32d865c27b565a6f593049de3a1873
jxke05050505 <jakehancox12@gmail.com>
2024-06-17 16:28:23 +0100

    add str func tests

--- html/phptest/test1.php
+++ html/phptest/test1.php
@@ -10,8 +10,21 @@
     <body>
         <h1>PHP Test 1</h1>
         <?php
-            $test = "this is a string variable";
-            echo($test);
+            $name = "Joe";
+            $text="Hello, world!";
+            $len=strlen($text);
+            $greet=str_replace("world",$name,$text);
+            echo strtoupper($text);
+            echo nl2br("\n");
+            echo strtolower($text);
+            echo nl2br("\n");
+            echo($greet);
+            echo nl2br("\n");
+            echo strrev($name." ".$greet);
+            echo nl2br("\n");
+            echo($greet);
+            echo nl2br("\n");
+            echo($len);
         ?>
     </body>
 </html>
