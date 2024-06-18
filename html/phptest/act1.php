ee11e9330b8cab1187a69cbe05ef725e867fce16
jxke05050505 <jakehancox12@gmail.com>
2024-06-18 15:45:32 +0100

    initial commit

---
+++ html/phptest/act1.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>PHP Activity 1</title>
+        <style>
+            body{
+                font-family: "segoe ui", 'inconsolata', consolas, monospace;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>PHP Activity 1</h1>
+        <?php
+            $pc=50;
+            if($pc>=70){
+                echo("First");
+            }
+            elseif($pc>=60){
+                echo("Upper Second");
+            }
+            elseif($pc>=50){
+                echo("Lower Second");
+            }
+            elseif($pc>=40){
+                echo("Third");
+            }
+            else{
+                echo("Fail");
+            }
+        ?>
+    </body>
+</html>
