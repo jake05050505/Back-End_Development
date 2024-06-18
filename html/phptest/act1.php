eecf270d561f466345cde87a8053d29fa2c47881
jxke05050505 <jakehancox12@gmail.com>
2024-06-18 15:50:41 +0100

    change output text

--- html/phptest/act1.php
+++ html/phptest/act1.php
@@ -11,6 +11,7 @@
         <h1>PHP Activity 1</h1>
         <?php
             $pc=50;
+            echo($pc. "% puts you in class:<br />");
             if($pc>=70){
                 echo("First");
             }
