6ca9bf1541c75f25868530f10109e14368a0834e
jxke05050505 <jakehancox12@gmail.com>
2024-06-18 17:09:55 +0100

    initial commit

---
+++ html/phptest/act2.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>PHP Activity 2</title>
+        <style>
+            body{
+                font-family: "segoe ui", 'inconsolata', consolas, monospace;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>PHP Activity 2</h1>
+        <form name="form" action="" method="get">
+            <input type="number" name="num1" id="num1" placeholder="num1:">
+            <input type="number" name="num2" id="num2" placeholder="num2:">
+            <input type="submit">
+        </form>
+        <?php
+            include("navbar.php");
+
+            $same=$_GET['num1']==$_GET['num2'];
+            if($same==1){
+                echo("It's the same");
+            } else{
+                echo("It's not the same");
+            }
+        ?>
+    </body>
+</html>
