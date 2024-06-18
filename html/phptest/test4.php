22a16c6831b0ef3e46d9af47f869040a9a9643fa
jxke05050505 <jakehancox12@gmail.com>
2024-06-18 14:51:16 +0100

    initial commit

--- html/phptest/test4.php
+++ html/phptest/test4.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>PHP Test 3</title>
+        <title>PHP Test 4</title>
         <style>
             body{
                 font-family: "segoe ui", 'inconsolata', consolas, monospace;
@@ -8,15 +8,20 @@
         </style>
     </head>
     <body>
-        <h1>PHP Test 3</h1>
+        <h1>PHP Test 4</h1>
         <?php
             $month = date_format(date_create(),"m");
 
             if ($month == "06"){
                 echo("It's June!");
+                echo("<br />");
             }
             if ($month == "07"){
-                echo("It's July");
+                echo("It's July!");
+                echo("<br />");
+            }
+            else{
+                echo("It is not July.");
             }
         ?>
     </body>
