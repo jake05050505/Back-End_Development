d382f70ee6fdfe49091cab8fe05686cb5990bf2e
jxke05050505 <jakehancox12@gmail.com>
2024-07-05 16:37:44 +0100

    initial commit

---
+++ html/sqltest/nav.php
@@ -0,0 +1,8 @@
+<a href="/sqltest/create.php">create</a>
+<span>|</span>
+<a href="/sqltest/get.php">read</a>
+<span>|</span>
+<a href="/sqltest/edit.php">update</a>
+<span>|</span>
+<a href="/sqltest/delete.php">delete</a>
+<br>
